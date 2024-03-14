5178fa6d980fb9b46bcf5971fc672a6647c0c773
abhnar0g <abhinandannarang3@gmail.com>
2024-03-15 01:02:10 +0530

    Adding Unique key generator for Teacher Service

---
+++ Teacher-Service/src/main/java/com/Skoolio/TeacherService/TeacherService/utils/UniqueIdGenerator.java
@@ -0,0 +1,28 @@
+package com.Skoolio.TeacherService.TeacherService.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class UniqueIdGenerator {
+    public static String generateUniqueId(String uniqueString) {
+        byte[] hash = computeSHA256Hash(uniqueString);
+        String base64urlHash = base64UrlEncode(hash);
+        return base64urlHash.substring(0, 10);
+    }
+
+    private static byte[] computeSHA256Hash(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(input.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String base64UrlEncode(byte[] bytes) {
+        String base64 = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+        return base64.replace("_", "").replace("-", ""); // Remove URL-unsafe characters
+    }
+}
